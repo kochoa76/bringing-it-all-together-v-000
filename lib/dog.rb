fe1f9647479eda5c613ac9139591d49fce4e527e
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-02 00:50:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,7 +76,7 @@ class Dog
       dog_data= dog[0]
       self.new(dog_data[0], dog_data[1], dog_data[2])
     else 
-      self.create(name, breed)
+      self.create(name:name, breed:breed)
     end 
   end 
   
